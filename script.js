cecc48ad8d71904af279caa81ce79fceb46ddb04
GilangDev-Web <158812882+GilangDev-Web@users.noreply.github.com>
2024-02-06 01:10:24 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function beliShow(produk2)
 {
-    var linkWhatsApp = 'https://wa.me/' + 6285771419637 + '?text=Saya%20tertarik%20dengan%20' + encodeURIComponent(produk2);
+    var linkWhatsApp = 'https://wa.me/' + 6285771419637 + '?text=Saya%20ingin%20beli%20' + encodeURIComponent(produk2);
     window.open(linkWhatsApp, '_blank')
 }
 
